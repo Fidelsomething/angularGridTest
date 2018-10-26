b2957b4ba9609a7cda8c7753ad7807f3132538d1
fidelsomething <filipe.teix@hotmail.com>
2018-10-26 15:44:15 +0100

    dropdown lists

---
+++ src/app/mali-navbar/mali-navbar.component.spec.ts
@@ -0,0 +1,42 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+import { MaliNavbarComponent } from './mali-navbar.component';
+
+describe('MaliNavbarComponent', () => {
+  let component: MaliNavbarComponent;
+  let fixture: ComponentFixture<MaliNavbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MaliNavbarComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MaliNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
